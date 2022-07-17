03360f3ada18a0c145f776443f45cc41b374eb73
Ductran216 <trantrungduc216@gmail.com>
2022-07-18 01:53:27 +0700

    Deploy

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,3 +1,6 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : '';
+export const apiUrl =
+    process.env.NODE_ENV !== 'production'
+        ? 'http://localhost:5000/api'
+        : 'https://enigmatic-stream-02119.herokuapp.com/api';
 
 export const LOCAL_STORAGE_TOKEN_NAME = 'mern-learnit';
